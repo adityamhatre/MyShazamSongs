c5f10545608c7d1467a3891e8756e78f0bdaed93
Aditya Mhatre <aditya.r.mhatre@gmail.com>
2020-01-19 15:24:25 -0600

    update logging

--- Logger.py
+++ Logger.py
@@ -1,40 +1,53 @@
 import datetime
 import time
 
+_info = 0
+_error = 0
+
 
 def error(message):
+    global _error
+    print(message,"lol")
     info("Some error occurred")
-    with open("error.log", "a") as err_file:
-        import time
-        import datetime
+    with open("error.html", "a") as err_file:
+        if _error == 0:
+            _error = 1
+            err_file.write('<meta http-equiv="refresh" content="2">\n\n\n<br>')
 
         ts = time.time()
         st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         err_file.write(st)
         err_file.write("\n===============================\n")
         err_file.write(message)
-        err_file.write("\n\n\n")
-    delete("error.log", 30)
+        err_file.write("\t\t\t<br><br><br>\n")
+
+    delete("error.html", days=30)
 
 
 def info(message):
-    with open("info.log", "a") as info_file:
-        import time
-        import datetime
+    global _info
+    with open("info.html", "a") as info_file:
+        if _info == 0:
+            _info = 1
+            info_file.write('<meta http-equiv="refresh" content="2">\n\n\n<br>')
 
         ts = time.time()
         st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         info_file.write(st)
         info_file.write(":\t")
         info_file.write(message)
-        info_file.write("\n")
+        info_file.write("\t\t\t\t\t\t<br>\n")
 
-    delete("info.log", days=7)
+    delete("info.html", days=7)
 
 
 def delete(file, days):
     with open(file, "r") as target_file:
-        first_date = target_file.readline().split(" ")[0]
+        splitted = target_file.readline().split('<meta http-equiv="refresh" content="2">\n\n\n')
+        if len(splitted) < 2:
+            return
+
+        first_date = target_file.readline().split('<meta http-equiv="refresh" content="2">\n\n\n')[1]
         today = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d')
 
         first_date = map(lambda x: int(x), first_date.split("-"))
